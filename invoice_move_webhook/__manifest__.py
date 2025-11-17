9a5e15369fed7c1cf02c724b75abe223929ed540
Bonnerue <thomas.bonnerue@akretion.com>
2025-11-17 15:38:13 +0100

    ADD webhook_invoice

---
+++ invoice_move_webhook/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2025 - TODAY, Akretion
+
+{
+    "name": "Invoice move webhook",
+    "version": "18.0.1.0.0",
+    "summary": "Gestion du webhook pour les factures",
+    "category": "Uncategorized",
+    "website": "https://github.com/akretion/sacpa-modules",
+    "license": "AGPL-3",
+    "author": " Akretion",
+    "depends": [
+        "base",
+        "account",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_move_view.xml",
+        "views/demat_link.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
