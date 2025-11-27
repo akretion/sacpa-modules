6346773c95aabde0aa925209257376f6b589b942
TB-Ph35 <thomas.bonnerue@phelumtech.fr>
2025-11-27 05:56:13 +0100

    fixup! Modification pour suivre les commandes

--- invoice_move_webhook/models/webhook.py
+++ invoice_move_webhook/models/webhook.py
@@ -1,13 +1,15 @@
 # Akretion Copyright (c) 2025 Author. All Rights Reserved.
 #
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class WebhookMove(models.Model):
     _name = "webhook.move"
 
-    name = fields.Char(string="Nome du lier webhook", compute="_compute_name")
+    name = fields.Char(
+        string="Nome du lier webhook", compute="_compute_name", store=True
+    )
 
     code_entite = fields.Char(string="code entité")
     entite = fields.Char(string="Nom de l'entité")
@@ -29,11 +31,12 @@ class WebhookMove(models.Model):
     adresse_de_livraison = fields.Char(string="adresse de livraison")
     commentaires = fields.Text(string="commentaires")
 
-    account_move_id = fields.Many2one("account.move")
+    account_move_id = fields.One2many("account.move", "webhook_id", "Facture lié")
     partner_id = fields.Many2one(
         "res.partner", string="Fournisseur", compute="_compute_fournisseur", store=True
     )
 
+    @api.depends("nom_fournisseur", "code_fournisseur")
     def _compute_fournisseur(self):
         for record in self:
             partner_id = record.env["res.partner"].search(
@@ -44,6 +47,7 @@ class WebhookMove(models.Model):
             )
             record.partner_id = partner_id.id
 
+    @api.depends("reference_de_la_commande")
     def _compute_name(self):
-        self.ensure_one()
-        self.name = "WBHK//" + self.reference_de_la_commande
+        for record in self:
+            record.name = "WBHK//" + record.reference_de_la_commande
